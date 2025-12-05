6ad6ea5281e717571dc3b0081da25f3927cac113
eisrael625 <117659847+eisrael625@users.noreply.github.com>
2025-12-04 23:00:53 -0500

    youtube

--- src/Youtube.js
+++ src/Youtube.js
@@ -1,36 +1,18 @@
+// Youtube.js
 import React from 'react';
-import YouTube from 'react-youtube';
-import './Youtube.css';
 
-const YouTubeVideo = ({ videoId }) => {
-  // Calculate dimensions based on viewport size
-  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-  const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-
-  // YouTube video options
-  const opts = {
-    height: `${vh * 0.67}`, // 67vh
-    width: `${vw * 0.55}`, // 55vw
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 0,
-    },
-  };
-  const optsMobile = {
-    width: `${vw * .9}`, // 95
-    height: `${vw * 0.7}`, // 25vh
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 0,
-    },
-  };
-
+function YouTubeVideo({ videoId }) {
   return (
-    <div>
-      <YouTube className='web' videoId={videoId} opts={opts} />
-      <YouTube className='mobile' videoId={videoId} opts={optsMobile} />
+    <div className="youtube-responsive">
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
     </div>
   );
-};
+}
 
 export default YouTubeVideo;
