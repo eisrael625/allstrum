875a62be7c5457810798275aaab6506d746755ad
eisrael625 <117659847+eisrael625@users.noreply.github.com>
2026-03-18 00:02:00 -0400

    New Logo

--- src/Header.js
+++ src/Header.js
@@ -26,20 +26,19 @@
 // export default Header;
 // Header.jsimport React from 'react';
 import './Header.css';
-import logo from './logo.png'; // ← your logo file
+import logo from './logo.png';
 
 function Header() {
   return (
     <header className="site-header">
       <div className="site-header-inner">
 
-        {/* BRAND + LOGO */}
+        {/* LOGO */}
         <a href="#products" className="brand">
-          <img src={logo} alt="AllStrum Logo" className="brand-logo" />
-          <span className="brand-text">AllStrum</span>
+          <img src={logo} alt="AllStrum" className="brand-logo" />
         </a>
 
-        {/* NAVIGATION */}
+        {/* NAV */}
         <nav className="site-nav">
           <a href="#products">Products</a>
           <a href="#story">Story</a>
@@ -54,3 +53,4 @@ function Header() {
 }
 
 export default Header;
+
